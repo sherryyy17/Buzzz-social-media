0e27cd24fd46a123ec100e128c445a2775dd89cb
Sherry Jain <sherryjain17@gmail.com>
2021-12-14 23:37:30 +0530

    cookie -session configured

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const authRoutes = require('./routes/authRoutes');
+const passport = require('passport');
+const { UserModel } = require('./models/User');
 require('./services/passport');
-const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
