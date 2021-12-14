784a751ce826822f1d403391d0dfc78e21fad18d
Sherry Jain <sherryjain17@gmail.com>
2021-12-15 01:16:09 +0530

    Integrated Google login button with backend

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,15 +1,20 @@
 import React from "react";
 import classes from './Login.module.css';
-import logo from '../../assets/logo.jpg'
+import logo from '../../assets/logo.jpg';
 
 const Login = () => {
+
+    const loginHandler = () => {
+        window.open('http://localhost:5000/auth/google',"_self");
+    }
+
     return (
         <div className={classes.container} >
             <div className={classes.innerContainer} >
                 <img src={logo} alt="TTN Logo" />
                 <h3>Enter your details and Start<br/> your journey with us</h3>
                 <p>Don't stop until you're proud</p>
-                <button>Sign in with Google</button>
+                <button onClick={ loginHandler }>Sign in with Google</button>
             </div>
         </div>
     )
