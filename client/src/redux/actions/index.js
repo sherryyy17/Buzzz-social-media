0954facf6aa6bd1f9aae3cd8eb00540d107f7e1b
Sherry Jain <sherryjain17@gmail.com>
2021-12-17 19:31:59 +0530

    Async action creater moved to index.js

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import { FETCH_USER } from './type';
+
+export const fetchUser = () =>
+    async (dispatch) => {
+        const res = await axios.get('/api/currUser');
+            
+        dispatch({ type: FETCH_USER, payload: res.data });
+    };
