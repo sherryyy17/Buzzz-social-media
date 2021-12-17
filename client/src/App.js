a43cdb2e5007ecdf0618357c4cc111976c199597
Sherry Jain <sherryjain17@gmail.com>
2021-12-17 19:33:16 +0530

    fetch_user action dispatched from App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Login from "./components/Login/Login";
 import Profile from "./components/Profile/Profile";
+import * as actions from './redux/actions';
 
-function App() {
+function App(props) {
+
+  useEffect( () => {
+    props.fetchUser();
+  },[]);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -14,4 +21,4 @@ function App() {
   );
 }
 
-export default App;
+export default connect(null, actions)(App);
