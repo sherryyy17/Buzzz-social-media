6a7f1d66071adace1aeb210a8d7c741d02930895
Sherry Jain <sherryjain17@gmail.com>
2021-12-20 19:57:33 +0530

    body-parser installed

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/users')
 const passport = require('passport');
-const { UserModel } = require('./models/User');
 require('./services/passport');
 var cors = require('cors');
 
@@ -12,6 +13,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json())
 app.use(cors());
 
 app.use(
@@ -24,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+app.use(userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log("Listening to port 5000"); })
