8fcdd3d6aa39924fc2211f3119310cf43c7ff987
Sherry Jain <sherryjain17@gmail.com>
2021-12-23 19:43:09 +0530

    added curr user  profile pic in header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 import logo from '../../assets/logo.png';
 import classes from './Header.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment, faUserFriends } from '@fortawesome/free-solid-svg-icons'
+import { faCommentDots, faUserFriends } from '@fortawesome/free-solid-svg-icons'
 
-
-const Header = () => {
-    const [ firstName, setFirstName ] = useState('');
-    const [ lastName, setLastName ] = useState('');
-
-    useEffect(() => {
-        console.log('xyzzzz');
-        axios.get('http://localhost:5000/currUser')
-            .then((res) => {
-                setFirstName(res.data.firstName);
-                setLastName(res.data.lastName);
-            })
-            .catch(err => console.log(err.message));
-    }, []);
+const Header = (props) => {
+    
+    let firstName = '', lastName = '', profilePic = '';
+    if(props.auth != null || props.auth){
+        firstName = props.auth.firstName;
+        lastName = props.auth.lastName;
+        profilePic = props.auth.profilePic;
+    }
 
     return <div className={classes.container}>
         <div>
             <img src={ logo } alt="ttn-logo" className={classes.logo} />
         </div>
         <nav>
-            <img src={ logo } alt="profile-pic" />
-            <span>{firstName} {lastName}</span>
-            <FontAwesomeIcon icon={faComment} className={classes.icons} />
+            <img src={ profilePic } alt="profile-pic" />
+            <span>{ firstName } { lastName }</span>
+            <FontAwesomeIcon icon={faCommentDots} className={classes.icons} />
             <FontAwesomeIcon icon={ faUserFriends } />
         </nav>
     </div>
 }
 
-export default Header;
+function mapStateToProps( { auth } ) {
+    return { auth };    
+}
+
+export default connect(mapStateToProps)(Header);
