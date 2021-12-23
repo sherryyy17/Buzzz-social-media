e5b38f2d53d9d868c912729e4ad5f5967a5791c5
Sherry Jain <sherryjain17@gmail.com>
2021-12-23 19:35:26 +0530

    Added friendIds in userSchema

--- models/User.js
+++ models/User.js
@@ -16,9 +16,14 @@ const UserSchema = new Schema({
     },
     email: String,
     profilePic: {
-        type: String,
-        required: true
-    }
+        type: String
+    },
+    address : {
+        country: String,
+        city: String
+    },
+    description : String,
+    friendsIds : [String]
 });
 
 const UserModel = mongoose.model('users', UserSchema);
