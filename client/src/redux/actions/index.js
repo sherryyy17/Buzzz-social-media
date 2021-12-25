2fe6c1b4a1db49cfaaff8b008243d6e217838a0a
Sherry Jain <sherryjain17@gmail.com>
2021-12-26 04:59:44 +0530

    Create post and get all posts actions created

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
-import { FETCH_BY_ID, FETCH_USER, UPDATE_USER, UPDATE_CURR_USER } from './type';
-import { FETCH_SUGGESTED } from './type';
+import { FETCH_USER, FETCH_SUGGESTED, UPDATE_USER, UPDATE_CURR_USER, SAVE_POST, FETCH_POST} from './type';
 
 export const fetchUser = () =>
     async (dispatch) => {
-        const res = await axios.get('/api/currUser');
-            
+        const res = await axios.get('/api/currUser');            
         dispatch({ type: FETCH_USER, payload: res.data });
-    };
+};
 
 export const fetchSuggestedUsers = () => 
     async (dispatch) => {
         const users = await axios.get('/api/users');
         dispatch({ type: FETCH_SUGGESTED, payload: users.data });
-    };
+};
 
 export const UpdateCurrUser = (id, data) => 
     async (dispatch) => {
@@ -25,4 +23,16 @@ export const UpdateUser = (id, data) =>
     async (dispatch) => {
         const user = await axios.patch(`/api/users/${ id }`, data);
         dispatch({ type: UPDATE_USER, payload: user.data });
-    };
+};
+
+export const fetchPost = () =>
+    async (dispatch) => {
+        const postList = await axios.get('/api/posts');
+        dispatch({ type: FETCH_POST, payload: postList.data });
+};
+
+export const SavePost = (data) => 
+    async (dispatch) => {
+        const posts = await axios.post(`/api/posts`, data);
+        dispatch({ type: SAVE_POST, payload: posts.data });
+};
--- client/src/redux/actions/type.js
+++ client/src/redux/actions/type.js
@@ -3,3 +3,5 @@ export const FETCH_SUGGESTED = 'fetch_suggested';
 export const UPDATE_USER = 'update_user';
 export const UPDATE_CURR_USER = 'update_user';
 export const FETCH_BY_ID = 'fetch_by_id';
+export const SAVE_POST = 'save_post';
+export const FETCH_POST = 'fetch_post';
