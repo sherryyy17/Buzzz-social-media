2f1657dfe5c296471e10668eb3361b804f7ad53d
Sherry Jain <sherryjain17@gmail.com>
2021-12-26 05:02:38 +0530

    posts routes created

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser')
 const keys = require('./config/keys');
 const cookieSession = require('cookie-session');
 const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/users')
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
 const passport = require('passport');
 require('./services/passport');
 var cors = require('cors');
@@ -27,6 +28,7 @@ app.use(passport.session());
 
 authRoutes(app);
 app.use(userRoutes);
+app.use(postRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { console.log("Listening to port 5000"); })
---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const postController = require('../controller/postController');
+
+router.post('/api/posts',postController.createPost);
+router.get('/api/posts',postController.getAllPosts);
+// router.get('/api/posts/:id', postController.getPostById);
+// router.patch('/api/posts/:id', postController.updatePost);
+// router.delete('/api/posts/:id', postController.deletePost);
+
+module.exports = router;
