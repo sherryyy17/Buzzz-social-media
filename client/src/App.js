67609dd9dee164cc344b1d7958c217a744e5ee4c
Sherry Jain <sherryjain17@gmail.com>
2021-12-26 05:00:43 +0530

    fetch post Action dispatched from App

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,16 @@ import { connect } from 'react-redux';
 import Login from "./components/Login/Login";
 import Profile from "./components/Profile/Profile";
 import * as actions from './redux/actions';
+import Suggestions from "./components/Suggestions/Suggestions";
+import Friends from "./components/Friends/Friends";
+import PostList from "./components/Posts/PostList";
 
 function App(props) {
 
   useEffect( () => {
     props.fetchUser();
+    props.fetchSuggestedUsers();
+    props.fetchPost();
   },[]);
 
   return (
@@ -16,6 +21,9 @@ function App(props) {
       <Routes>
         <Route path='/' element={ <Login /> } exact />
         <Route path='/profile' element= { <Profile /> } />
+        <Route path='/suggestion' element= { <Suggestions /> } />
+        <Route path='/friends' element= { <Friends /> } />
+        <Route path='/post' element = { <PostList /> } />
       </Routes>
     </BrowserRouter>
   );
